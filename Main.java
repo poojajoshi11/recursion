82d463792c91451b1651a813e3061fb0020ad47d
Pooja Joshi <pjoshi01@mail.uoguelph.ca>
2018-12-03 18:42:31 -0500

    test

---
+++ Main.java
@@ -0,0 +1,25 @@
+package random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter number of steps in a stairwell: ");
+        int steps = in.nextInt();
+        System.out.println("Ways = " + countWays(steps));
+
+    }
+    public static int countWays(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+        return countWays(n - 2) + countWays(n - 1);
+    }
+}
